56b54da861a1e78543c0fd3a33b649b32bf4ec80
nikita <nikita.yazkov@yandex.ru>
2024-06-12 17:29:21 +0300

    Телефонная книга, в которой контакты отсортированы сперва по имени, а затем по фамилии.

---
+++ SortedPhoneBook/Contact.cs
@@ -0,0 +1,43 @@
+﻿namespace SortedPhoneBook
+{
+    /// <summary>
+    /// Контакт телефонной книги
+    /// </summary>
+    internal class Contact
+    {
+        /// <summary>
+        /// Параметризированный конструктор
+        /// </summary>
+        /// <param name="name"><inheritdoc cref="Name" path="/summary"/></param>
+        /// <param name="lastName"><inheritdoc cref="LastName" path="/summary"/></param>
+        /// <param name="phoneNumber"><inheritdoc cref="PhoneNumber" path="/summary"/></param>
+        /// <param name="email"><inheritdoc cref="Email" path="/summary"/></param>
+        public Contact(string name, string lastName, long phoneNumber, string email)
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string LastName { get; }
+
+        /// <summary>
+        /// Номер телефона
+        /// </summary>
+        public long PhoneNumber { get; }
+
+        /// <summary>
+        /// Почта
+        /// </summary>
+        public string Email { get; }
+    }
+}
--- SortedPhoneBook/Program.cs
+++ SortedPhoneBook/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SortedPhoneBook
 {
     class Program
     {
+        /// <summary>
+        /// Телефонная книга
+        /// </summary>
+        private static readonly List<Contact> _phoneBook = new List<Contact>
+        {
+            new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"),
+            new Contact("Сергей", "Довлатов",79990000010, "serge@example.com"),
+            new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"),
+            new Contact("Валерий", "Леонтьев",79990000012, "valera@example.com"),
+            new Contact("Сергей", "Брин",  799900000013, "serg@example.com"),
+            new Contact("Иннокентий", "Смоктуновский",799900000013, "innokentii@example.com")
+        }.OrderBy(contact => contact.Name).ThenBy(contact => contact.LastName).ToList();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("Введите номер страницы: ");
+
+                var input = Console.ReadKey().KeyChar;
+
+                var parsed = int.TryParse(input.ToString(), out int pageNumber);
+
+                if (!parsed || pageNumber < 1 || pageNumber > 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Страницы не существует");
+                }
+                else
+                {
+                    var pageContent = _phoneBook.Skip((pageNumber - 1) * 2).Take(2);
+                    Console.WriteLine();
+
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+                }
+
+                Console.Write("Для выхода нажмите 'x': ");
+
+                var exitChar = Console.ReadKey().KeyChar;
+                if (exitChar.Equals('x'))
+                    break;
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
