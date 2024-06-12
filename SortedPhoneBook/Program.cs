47129152c9c1436d05bf3fdd9e60c37356e90c01
nikita <nikita.yazkov@yandex.ru>
2024-06-12 16:58:49 +0300

    Add project files.

---
+++ SortedPhoneBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortedPhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
